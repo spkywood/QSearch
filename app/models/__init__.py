09e9e5409677f74928aafd669eea9ae0c51c5e6b
longfellow <longfellow.wang@gmail.com>
2024-06-17 13:19:46 +0800

    新增对话模型

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,5 +4,6 @@ from .user import User
 from .role import Role
 from .permission import Permission
 from .origanization import Origanization
+from .conversation import Conversation
 from .role_permission import role_permission_association
 from .user_org_role import user_org_role
---
+++ app/models/conversation.py
@@ -0,0 +1,24 @@
+#! python3
+# -*- encoding: utf-8 -*-
+'''
+@File    : conversation.py
+@Time    : 2024/06/17 11:11:44
+@Author  : longfellow
+@Version : 1.0
+@Email   : longfellow.wang@gmail.com
+'''
+
+
+from app.models import BaseTable
+
+from sqlalchemy import String, Integer, JSON, ForeignKey
+from sqlalchemy.orm import (
+    relationship, mapped_column, Mapped
+)
+
+class Conversation(BaseTable):
+    __tablename__ = "conversations"
+
+    topic_name: Mapped[str] = mapped_column(String(255), nullable=True, default='default')
+    user_id: Mapped[int] = mapped_column(Integer)
+    history: Mapped[dict] = mapped_column(JSON)
