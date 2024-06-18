d51f1f16dd5aa49d8117454ebb6314757f1a3ab9
spkywood <longfellow.wang@gmail.com>
2024-06-18 16:36:59 +0800

    新增milvus操作封装

--- db/milvus.py
+++ db/milvus.py
@@ -9,7 +9,7 @@
 '''
 
 
-from typing import List
+from typing import List, Dict
 from pymilvus import (
     connections,
     DataType,
@@ -20,8 +20,9 @@ from pymilvus import (
     CollectionSchema,
 )
 from common.singleton import Singleton
+from common import logger
 
-class Milvus(metaclass=Singleton):
+class MilvusStore(metaclass=Singleton):
     """
     Milvus 存储 查询
     """
@@ -31,7 +32,7 @@ class Milvus(metaclass=Singleton):
         fields = [
             FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
             FieldSchema(name="kb_name", dtype=DataType.VARCHAR, max_length=100, is_partition_key=True),
-            FieldSchema(name="chunk", dtype=DataType.VARCHAR, max_length=20000),
+            FieldSchema(name="chunk_text", dtype=DataType.VARCHAR, max_length=20000),
             FieldSchema(name="chunk_uuid", dtype=DataType.VARCHAR, max_length=100),
             FieldSchema(name="vector", dtype=DataType.FLOAT_VECTOR, dim=dim),
             FieldSchema(name="metadata", dtype=DataType.JSON)
@@ -39,24 +40,82 @@ class Milvus(metaclass=Singleton):
 
         return CollectionSchema(fields=fields, description="Chunk Vector Collection.")
     
-    def __init__(self, host: str = "localhost", port: int = 19530):
+    def __init__(self, host, port) -> None:
         self.client = MilvusClient(f"http://{host}:{port}")
         connections.connect(host=host, port=port)
+
+    def has_collection(self, collection_name) -> bool:
+        return self.client.has_collection(collection_name)
     
-    def create_collection(self, collection_name: str, dim: int, metric_type: str = "L2") -> Collection:
-        schema = Milvus.create_schema(dim)
+    def get_collection(self, collection_name) -> Collection:
+        has = self.client.has_collection(collection_name)
+        if has:
+            # connections.connect()
+            return Collection(collection_name)
+        else:
+            raise MilvusException(
+                code=404, message=f"Collection {collection_name} already exists")
 
-        index_params = self.client.prepare_index_params(
-            "vector", metric_type=metric_type, index_type="IVF_FLAT", nlist=1024
-        )
+    def list_collections(self) -> List[str]:
+        return self.client.list_collections()
 
-        return self.client.create_collection(collection_name, schema, index_params=index_params)
+    def create_collection(self, collection_name, dim, metric_type="L2"):
+        has = self.client.has_collection(collection_name)
+        if has:
+            logger.info(f"Collection {collection_name} already exists")
+            # raise MilvusException(
+            #     code=510, message=f"Collection {collection_name} already exists")
+        else: 
+            schema = MilvusStore.create_schema(dim)
+            index_params = self.client.prepare_index_params("embedding", metric_type=metric_type)
+
+            return self.client.create_collection(collection_name, schema=schema, index_params=index_params)
+
+    def insert(self, collection_name: str, entities: List):
+        if not self.has_collection(collection_name):
+            raise MilvusException(
+                code=404, message=f"Collection {collection_name} does not exist"
+            )
+        self.client.insert(collection_name, entities)
+
+    def insert_entities_with_collection(self, collection: Collection, entities: List):
+        collection.insert(entities)
+
+    def query_entities(self, collection_name: str, filter: str, top_k: int) -> List[Dict]:
+        if not self.has_collection(collection_name):
+            raise MilvusException(
+                code=404, message=f"Collection {collection_name} does not exist"
+            )
+        
+        
+        return self.client.query(collection_name, filter=filter, top_k=top_k)
     
-    def insert(self, collection_name: str, entities: List) -> int:
-        pass
+    """
+    TODO: 布尔表达式
+        https://www.milvus-io.com/boolean
+    """
+    def search(self, collection_name: str, 
+               query_embedding: List,
+               top_k: int = 3,
+               expr: str = None,
+               consistency_level: str = "Eventually") -> List[Dict]:
+        if not self.has_collection(collection_name):
+            raise MilvusException(
+                code=404, message=f"Collection {collection_name} does not exist"
+            )
 
-    def search(self, collection_name: str, query_vector: List,
-               top_k:int = 3, kb_names: List[str] = None,
-               expr:str = None,
-               consistency_level:str = "Eventually",) -> List[str]:
-        pass
+        collection = self.get_collection(collection_name)
+        collection.load()
+        search_param = {
+            "expr": expr,
+            "data": [query_embedding],
+            "anns_field": "embedding",
+            "param": {"metric_type": "L2", "params": {"nprobe": 10}},
+            "limit": top_k,
+            "output_fields": ["uuid"],
+            "consistency_level": consistency_level
+        }
+        logger.info("search..." if expr is None else "hybrid search...")
+        
+        results = collection.search(**search_param)
+        return [r.to_dict().get("entity").get("uuid") for result in results for r in result]
